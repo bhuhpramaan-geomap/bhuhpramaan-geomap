7d709fd6321715036aa4b3a6a912654f60b41168
rajuceg <RajuCeg@outlook.com>
2024-06-28 14:49:03 +0530

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -2,39 +2,21 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 
 markdown = """
-Our Website
-<https://www.bhuhpramaan.com>
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/MWpI4OI.jpeg"
+logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.markdown(
-    """
-    <style>
-    .main .block-container {
-        padding-top: 10px; /* Adjust the value as needed */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.title("LAND USE AND LAND COVER (LULC) OF BANGALORE CITY -A GEOSPATIAL APPROACH")
 
 st.title("Interactive Map")
 
-st.markdown(
-    """
-    The Interactive Map allows you to engage with the map in various ways, such as zooming in and out, and has various basemap. The basemap allows the user to get more information about the Bangalore Map.
-    """
-)
-
 col1, col2 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
-index = options.index("TERRAIN")
+index = options.index("OpenTopoMap")
 
 with col2:
 
@@ -43,17 +25,8 @@ with col2:
 
 with col1:
 
-    m = leafmap.Map(center=[12.971599,77.594566], zoom=9.8,
+    m = leafmap.Map(
         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
     )
     m.add_basemap(basemap)
-    m.to_streamlit(height=800)
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+    m.to_streamlit(height=700)
