9e8275d034d0a47732bc4a3076eb2c090fffeb18
V Raju <Rajuceg@outlook.com>
2024-07-13 11:58:56 +0530

    add to home

--- Home.py
+++ Home.py
@@ -25,7 +25,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title("LAND USE AND LAND COVER (LULC) OF BANGALORE CITY -A GEOSPATIAL APPROACH")
+st.title("Land use and Land classification of Bangalore")
 
 st.markdown(
     """
@@ -49,7 +49,8 @@ m.to_streamlit(height=500)
 
 st.header("Bangalore")
 
-markdown = """Land-use and land-cover change (LULCC); is a general term for the human modification of Earth's terrestrial surface. Bangalore City is one of the rapidly growing city in India as well as in Asia. The Growth of the Bangalore city took its speed after 2000-23, because of Real Estate, Globalization and its policies. Bangalore city is the part of Bangalore Urban District, the city has 709 KM2 Geographical area but the urban district has 2196KM2. The population of the study area crosses over 105 million in 2017. The Population and the City growth mainly affected the LULC of the area. Land Use and Land Cover look like similar words but while studying deeply we can understand LULC concept easily. For the study purpose only Urban and Urban influence area have been taken. The major aim of the research paper is to show the changes occur in the LULC of Bangalore City using Remote Sensing Technology. Multi- Spectral Satellite Imageries are used to know the development and changes taken in Land Use and Land Cover. GIS and ERDAS Technologies are used to do mapping and analysis. The research paper mainly concentrating on City and BBMP region but some packages of land have been used to understand the influencing zone of Urban and Rural"""..
+markdown = """
+Bengaluru is the capital and largest city of the southern Indian state of Karnataka. It has a population of more than 8 million and a metropolitan population of around 15 million, making it India's third most popular city and fourth most popular urban city. Bangalore spans an area of approximately 741 square kilometers (286 square miles). Including the urban and suburban areas, the Bangalore metropolitan region covers about 8,005 square kilometers (3,090 square miles). It is the most popular city and largest urban city in South India, and is the 27th largest city in the world. Located on the Deccan Plateau, at a height of over 900 m (3,000 ft) above sea level, Bangalore has a pleasant climate throughout the year, with its parks and green spaces earning it the reputation of India's "Garden City". Its elevation is the highest of India's major cities.
 
 Bangalore, known as the "Silicon Valley of India," is the country's top IT exporter. It is home to notable technological organizations including ISRO, Infosys, Wipro, and HAL. As India's second fastest-growing major metropolis, Bangalore boasts numerous educational and research institutions, such as IISc, IIMB, NIFT, NID, NLSIU, and NIMHANS. Additionally, the city hosts state-owned aerospace and defense entities like Bharat Electronics, Hindustan Aeronautics, and National Aerospace Laboratories, as well as the Kannada film industry.
 
@@ -57,7 +58,7 @@ Agriculture:
 
 Bangalore, surrounded by fertile rural areas, is a hub for horticulture, silk production, and dairy farming. The region is known for cultivating fruits, vegetables, flowers, and key crops like ragi and maize. Bangalore also hosts prominent agricultural research institutions like the University of Agricultural Sciences, fostering innovations in farming techniques and crop improvement. Despite challenges from urbanization and water scarcity, government initiatives support local farmers with subsidies, training, and market access, maintaining the area's significant role in agriculture.
 
-Bangalore Ward:"""
+Places with Pincode, Latitude and Longitude:"""
 
 st.markdown(markdown)
 
